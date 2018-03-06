b081c1b1122d24a20d177d99e55342b06fbc83b1
Stas <stanislau.luzanau@clarabridge.com>
2018-03-06 17:15:41 +0300

    Initial Commit
    Initial Commit

---
+++ routes/user.ts
@@ -0,0 +1,45 @@
+﻿/*
+ * GET users listing.
+ */
+import express = require('express');
+const router = express.Router();
+import { UserStore } from '../src/db';
+import * as Promise from 'bluebird';
+import * as util from 'util';
+import { User } from '../src/user';
+const storeUser = new UserStore();
+
+router.post('/create', (req: express.Request, res: express.Response) => {
+    const user = new User(req.body);
+    return storeUser.createUser(user)
+        .then(result => res.status(200).send({ message: result }))
+        .catch(err => res.status(409).send({ error: err }));
+});
+
+router.get('/:username', (req: express.Request, res: express.Response) => {
+    const user = req.params['username'];
+    const apiKey = req.query['apiKey'];
+    return storeUser.validateApiKey(apiKey)
+        .then(() => storeUser.getUserByName(user))
+        .then(result => res.status(200).send(result))
+        .catch(err => res.status(400).send({ error: err }));
+});
+
+router.get('/', (req: express.Request, res: express.Response) => {
+    const apiKey = req.query['apiKey'];
+    return storeUser.validateApiKey(apiKey)
+        .then(() => storeUser.getAllUsers())
+        .then(result => res.status(200).send(result))
+        .catch(err => res.status(400).send({ error: err }));
+});
+
+router.delete('/:username', (req: express.Request, res: express.Response) => {
+    const user = req.params['username'];
+    const apiKey = req.query['apiKey'];
+    return storeUser.validateApiKey(apiKey)
+        .then(() => storeUser.deleteUser(user))
+        .then(result => res.status(200).send({ message: result }))
+        .catch(err => res.status(400).send({ error: err }));
+});
+
+export default router;
---
+++ src/user.ts
@@ -0,0 +1,24 @@
+﻿import * as crypto from 'crypto-js';
+
+export class User {
+
+    public userName: string;
+    public apiKey: string;
+
+    constructor(data: any) {
+        this.userName = data['userName'];
+        this.apiKey = this.generateApiKey(data);
+    }
+
+    public generateApiKey(data: any): string {
+        if (data['apiKey']) {
+            let key = data['apiKey'];
+            return key;
+        } else {
+            let date = (new Date).getTime();
+            let key = crypto.MD5(date + data['userName']).toString();
+            return key;
+        }
+    };
+
+};
